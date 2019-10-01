a20428ca635d142cb2b220c9b33d90c8b8d5e0d5
ASHIQ PS <ashiqps97@gmail.com>
2019-10-01 22:29:24 +0530

    Successfully Completed my work.

---
+++ DayShopBackend/src/main/java/aqz/project/DayShopBackend/App.java
@@ -0,0 +1,13 @@
+package aqz.project.DayShopBackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
